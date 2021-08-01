4c515f647ffe4d9e4279907096f300d4281c2d08
Capabay <capabay@hotmail.com>
2021-08-01 04:26:02 +0100

    List of function and methods

---
+++ heap.go
@@ -0,0 +1,25 @@
+/*
+Steps to achieve this solution
+1. Make a struct for maximum heap
+2. Write an insert method
+3. Write an extract method
+In summary
+Struct -- MaxHeap
+Method -- Insert
+method -- Extract
+method -- maxHeapifyUp
+method -- maxHeapifyDown
+method -- Swap
+function -- Parent
+function -- Left
+function -- right 
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Hello heap")
+}
